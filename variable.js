ac67c07484b3bc22571d0d229b3b610ba5e2dc2d
Asfand Wakeel <asfandwakeel008@gmail.com>
2026-06-29 13:03:34 +0500

    variable exercise

---
+++ variable.js
@@ -0,0 +1,41 @@
+// let variable
+
+let studentName = "Asfand";
+let age = 22;
+
+console.log(studentName);
+console.log(age);
+
+
+//const variable
+const country = "Pakistan";
+
+console.log(country);
+
+
+//var variable
+var city = "Lahore";
+
+city = "Islamabad";
+
+console.log(city);
+
+
+
+// let variable
+
+let studentName = "Asfand";
+let age = 22;
+
+console.log(studentName);
+console.log(age);
+
+
+//const variable
+const country = "Pakistan";
+
+console.log(country);
+
+
+//var variable
+var city = "Lahore";
