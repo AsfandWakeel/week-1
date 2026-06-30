b6b0447795666f6d22f258b4a58a50228d67fcfd
Asfand Wakeel <asfandwakeel008@gmail.com>
2026-06-30 18:49:31 +0500

    Added setTimeout exercises

--- variable.js
+++ variable.js
@@ -22,20 +22,3 @@ console.log(city);
 
 
 
-// let variable
-
-let studentName = "Asfand";
-let age = 22;
-
-console.log(studentName);
-console.log(age);
-
-
-//const variable
-const country = "Pakistan";
-
-console.log(country);
-
-
-//var variable
-var city = "Lahore";
